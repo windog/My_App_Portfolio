9fd35dcffec0851c220ffc6568f93a74656a7479
windog <windiscall@gmail.com>
2016-04-23 21:01:34 +0800

    以后按钮多于三个的话，就在同一个onClick方法实现

--- app/src/main/java/com/udacity/windy/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/windy/myappportfolio/MainActivity.java
@@ -6,61 +6,70 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button spotify;
+    Button scores;
+    Button library;
+    Button build;
+    Button reader;
+    Button capstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button spotify = (Button) findViewById(R.id.spotify_btn);
-        spotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        spotify = (Button) findViewById(R.id.spotify_btn);
+        spotify.setOnClickListener(this);
+
+        scores = (Button) findViewById(R.id.scores_btn);
+        scores.setOnClickListener(this);
+
+
+        library = (Button) findViewById(R.id.library_btn);
+        library.setOnClickListener(this);
+
+
+        build = (Button) findViewById(R.id.build_btn);
+        build.setOnClickListener(this);
+
+        reader = (Button) findViewById(R.id.reader_btn);
+        reader.setOnClickListener(this);
+
+        capstone = (Button) findViewById(R.id.capstone_btn);
+        capstone.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.spotify_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my spotify app",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button scores = (Button) findViewById(R.id.scores_btn);
-        scores.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.scores_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my scores app",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        Button library = (Button) findViewById(R.id.library_btn);
-        library.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.library_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my library app",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        Button build = (Button) findViewById(R.id.build_btn);
-        build.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.build_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my build app",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button reader = (Button) findViewById(R.id.reader_btn);
-        reader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.reader_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my Reader app",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button capstone = (Button) findViewById(R.id.capstone_btn);
-        capstone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.capstone_btn:
                 Toast.makeText(MainActivity.this,"This button will launch my capstone app",Toast.LENGTH_SHORT).show();
-            }
-        });
+                break;
+            default:
+                break;
+        }
+
     }
 }
